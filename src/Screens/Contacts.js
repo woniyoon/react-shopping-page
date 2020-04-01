0ee61777d821a309c2e76d1bba7d2e9f784cec50
woniyoon <woniyoon@gmail.com>
2020-04-01 22:32:10 +0900

    Create About.js and Contacts.js

---
+++ src/Screens/Contacts.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import location from '../imgs/contacts_location.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+
+
+function Contacts() {
+    return (
+        <div>
+            <div style={{ backgroundColor: "red", marginTop: "100px" }}>
+                <h1>Contacts</h1>
+                <img src={location} width="800px" height="550px" alt="offline_shop_image" />
+            </div>
+            <div style={{ backgroundColor: "red", marginTop: "10px" }}>
+                <h4>Offline Shop</h4>
+                <p>470 Sunset Drive Scarsdale, NY 10583</p>
+                <h4>Phone</h4>
+                <p>716-354-3749</p>
+                <h4>Online Contacts</h4>
+
+                <i class="fab faFacebookF"></i>
+                {/* <p>470 Sunset Drive Scarsdale, NY 10583</p>
+                    <form>
+                        <label style={{ padding: "10px" }} >
+                            Name :  
+                        </label>
+                        <input type="text" name="name" />
+                        <input type="submit" value="Submit" />
+                    </form>
+                    <form>
+                        <label style={{ padding: "10px" }} >
+                            E-mail :  
+                        </label>
+                        <input type="text" name="name" />
+                        <input type="submit" value="Submit" />
+                    </form> */}
+                </div>
+        </div>
+    );
+}
+
+export default Contacts;
